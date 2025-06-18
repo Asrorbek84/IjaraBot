6021f87996f474c1a01fd0cf771303dcf299f637
Asrorbek84 <xaydarovasrorbek8425@gmail.com>
2025-06-18 09:27:52 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID")
-
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=["start"])
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Assalomu alaykum! IjaraBotga xush kelibsiz.")
+    await message.reply("Assalomu alaykum! IjaraBot ga xush kelibsiz!")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
